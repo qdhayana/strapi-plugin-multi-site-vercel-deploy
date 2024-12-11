43ac0533fa385ef70385ff732d793b533ff2f13a
fathurayana <faturrahman.qarnain@ayana.com>
2024-12-11 14:45:56 +0800

    fix: add translation json

--- admin/src/pages/App/index.jsx
+++ admin/src/pages/App/index.jsx
@@ -11,10 +11,10 @@ import pluginId from '../../pluginId';
 import HomePage from '../HomePage';
 import { Page } from "@strapi/strapi/admin";
 import { IntlProvider } from 'react-intl';
-
+import en from "../../translations/en.json"
 const App = () => {
   return (
-    <IntlProvider locale="en">
+    <IntlProvider locale="en" messages={en}>
       <div>
         <Routes>
           <Route path={`/plugins/${pluginId}`} element={<HomePage />} />
